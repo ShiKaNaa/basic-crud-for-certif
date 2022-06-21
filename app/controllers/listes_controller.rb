fc7c3ce62f112dd3338134f38f72e9e871191ebe
Raphael HOUDRY <raphael-59200@hotmail.fr>
2022-06-21 18:35:26 +0200

    destroy working

--- app/controllers/listes_controller.rb
+++ app/controllers/listes_controller.rb
@@ -4,10 +4,26 @@ class ListesController < ApplicationController
   end
 
   def new
-
+    @liste = Liste.new
   end
 
   def create
+    liste = Liste.new(liste_params)
+    if liste.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
 
+  def destroy
+    Liste.find(params[:id]).destroy
+    redirect_to root_path
+  end
+
+  private
+
+  def liste_params
+    params.require(:liste).permit(:nom)
   end
 end
